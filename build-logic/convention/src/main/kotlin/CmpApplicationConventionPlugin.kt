386d64726a8bd9824f78e3cae51b31c1fc3a958d
Vitalikas <kolinkv@gmail.com>
2026-02-04 15:41:16 +0200

    Feat: Create `cmp-application` convention plugin
    This commit introduces a new convention plugin, `lt.vitalijus.convention.cmp.application`, to encapsulate the build logic for the main Compose Multiplatform application module.
    
    ### Key Changes:
    
    *   **New Convention Plugin**:
        *   Created `CmpApplicationConventionPlugin` to apply and configure the `kotlin.multiplatform`, `compose.multiplatform`, and Android Compose plugins.
        *   This plugin also adds the Compose UI tooling dependency for debug builds.
    
    *   **Platform Target Configuration**:
        *   Introduced `configureAndroidTarget()` and `configureIosTargets()` extension functions within the `build-logic` module to standardize the configuration for Android and iOS targets (`iosX64`, `iosArm64`, `iosSimulatorArm64`).
        *   The framework `baseName` for iOS is set to `ComposeApp`.
        *   The JVM target for the Android target compiler options is now set to version 17.
    
    *   **Gradle Refactoring**:
        *   The `composeApp/build.gradle.kts` file has been simplified to use the new `convention.cmp.application` plugin, removing the now-redundant manual configuration for KMP targets and dependencies.
        *   Removed unused `jvmMain` source set configuration.
        *   The new plugin is registered in `build-logic/convention/build.gradle.kts` and added to `gradle/libs.versions.toml`.

---
+++ build-logic/convention/src/main/kotlin/CmpApplicationConventionPlugin.kt
@@ -0,0 +1,27 @@
+import lt.vitalijus.chirp.convention.configureAndroidTarget
+import lt.vitalijus.chirp.convention.configureIosTargets
+import lt.vitalijus.chirp.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class CmpApplicationConventionPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("lt.vitalijus.convention.android.application.compose")
+                apply("org.jetbrains.kotlin.multiplatform")
+                apply("org.jetbrains.compose")
+                apply("org.jetbrains.kotlin.plugin.compose")
+            }
+
+            configureAndroidTarget()
+            configureIosTargets()
+
+            dependencies {
+                "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
+            }
+        }
+    }
+}
